d38bfcbaab6cdd5afe98478d225879c28a234963
OmarL51 <omar.lozano@distrimedical.com.co>
2023-05-03 16:02:08 -0500

    Cambiossdistrime

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  private counter: number = 1;
+  constructor()
+  {
+    setInterval(() => {
+      const radio = document.getElementById(`radio${this.counter}`);
+      if (radio) {
+        (radio as HTMLInputElement).checked = true;
+      }
+      this.counter++;
+      if (this.counter > 4) {
+        this.counter = 1;
+      }
+    }, 5000);
+  }
+  }
 
-}
+
